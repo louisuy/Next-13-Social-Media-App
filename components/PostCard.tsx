20982bea7d41c14fc1bb5889fa607cbd6daf4860
Anthony Bon Louis Uy Cubillas <66798713+louisuy@users.noreply.github.com>
2023-06-06 17:56:53 +0400

    PostCard.tsx updated with user info.

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,3 +1,7 @@
+import { clerkClient } from "@clerk/nextjs";
+import Image from "next/image";
+import { use } from "react";
+
 type PostCardProps = {
     id: string;
     createdAt: Date;
@@ -5,12 +9,27 @@ type PostCardProps = {
     authorId: string;
 }
 
-export function PostCard({ content, authorId }: PostCardProps) {
+export async function PostCard({ content, authorId }: PostCardProps) {
+    const user = await clerkClient.users.getUser(authorId);
     return (
         <div
-            className='bg-white/[0.1] rounded-lg p-4 shadow-md hover:shadow-xl m-4 transition duration-200'
+            className='bg-white/[0.1] flex gap-4 rounded-lg p-4 shadow-md hover:shadow-xl m-4 transition duration-200'
         >
-            {content}
+            <div>
+                <Image
+                    src={user?.imageUrl}
+                    alt={user?.firstName}
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                />
+            </div>
+            <div className="flex flex-col">
+                <div>
+                    <p className="font-bold">{user?.firstName}</p>
+                    <p>{content}s</p>
+                </div>
+            </div>
         </div>
     )
 }
